02d272bbfcdaee85788d12db3811c766fb3d6e33
slievr <me@ethanbrodie.com>
2017-12-17 15:25:01 +0000

    currently experiencing errors

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use std::collections::HashMap;
+
 use rocket_contrib::Template;
 
 #[get("/")]
 fn index() -> Template {
-    //let context = /* object-like value */;
-    //Template::render("index", &context)
-    Template::render("index")
+    
+    let context = TemplateContext {
+        name: "name",
+        items: vec!["One", "Two", "Three"].iter().map(|s| s.to_string()).collect()
+    };
+    Template::render("index", &context)
 }
 
 fn main() {
