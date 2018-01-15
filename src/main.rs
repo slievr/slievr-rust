decc6b1ae4c5e77d295b0824aa303eef5a49a674
slievr <pacpellet@gmail.com>
2018-01-15 15:27:23 +0000

    Like example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
 extern crate rocket_contrib;
+extern crate rocket;
+#[macro_use] extern crate serde_derive;
 
-use std::collections::HashMap;
+#[cfg(test)] mod tests;
 
+use rocket::Request;
+use rocket::response::Redirect;
 use rocket_contrib::Template;
 
+#[derive(Serialize)]
+struct TemplateContext {
+    name: String,
+    items: Vec<String>
+}
+
 #[get("/")]
-fn index() -> Template {
-    
+fn index() -> Redirect {
+    Redirect::to("/hello/Unknown")
+}
+
+#[get("/hello/<name>")]
+fn get(name: String) -> Template {
     let context = TemplateContext {
-        name: "name",
+        name: name,
         items: vec!["One", "Two", "Three"].iter().map(|s| s.to_string()).collect()
     };
+
     Template::render("index", &context)
 }
 
+#[error(404)]
+fn not_found(req: &Request) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().as_str());
+    Template::render("error/404", &map)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, get])
+        .attach(Template::fairing())
+        .catch(errors![not_found])
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
